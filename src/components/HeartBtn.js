b58622b6240ea44074b81038d90a64542f4c3fe7
qorbaxk <bekgyber3@hanmail.net>
2022-10-14 16:48:57 +0900

    2.9 찜기능 오류 수정 및 활성화

--- src/components/HeartBtn.js
+++ src/components/HeartBtn.js
@@ -2,35 +2,63 @@ import React, { useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus, faCheck } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
-import { setDoc, doc, deleteDoc } from "firebase/firestore";
+import {
+  collection,
+  onSnapshot,
+  query,
+  DocumentSnapshot,
+  setDoc,
+  doc,
+  deleteDoc,
+  getDoc,
+} from "firebase/firestore";
 import { dbService } from "fBase";
 import { getAuth } from "firebase/auth";
-import { async } from "@firebase/util";
 
 const HeartBtn = ({ item }) => {
-  //true 면 찜한영화 false면 찜하지 않은 영화
-  const [heart, setHeart] = useState(false);
+  //true 면 찜하지 않은 영화 false면 찜한영화
+  const [heart, setHeart] = useState(null);
   const listObj = item;
   const auth = getAuth();
 
+  //찜 추가
   const putMyList = async () => {
     await setDoc(doc(dbService, auth.currentUser.uid, listObj.title), listObj);
   };
 
+  //찜 삭제
   const deleteMyList = async () => {
     await deleteDoc(doc(dbService, auth.currentUser.uid, listObj.title));
   };
 
-  useEffect(() => {
-    if (heart) {
-      putMyList(); //찜했을 때
+  //찜 확인
+  const existMyList = async () => {
+    const userRef = doc(dbService, auth.currentUser.uid, listObj.title);
+    const userSnap = await getDoc(userRef);
+    if (userSnap.exists()) { 
+      setHeart(true);
     } else {
-      deleteMyList(); //찜에서 제거
+      setHeart(false);
     }
-  }, [heart]);
+  };
+
+  //찜 클릭시
+  const onClick = () => {
+    setHeart((prev) => !prev);
+
+    if (heart) {
+      deleteMyList(); //찜에서 제거
+    } else {
+      putMyList(); //찜했을 때
+    }
+  };
+
+  useEffect(() => {
+    existMyList();
+  }, []);
 
   return (
-    <div className="heartbtn-area" onClick={() => setHeart((prev) => !prev)}>
+    <div className="heartbtn-area" onClick={onClick}>
       {heart ? (
         <FontAwesomeIcon icon={faCheck} size="2x" color="red" />
       ) : (
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,9 +1,28 @@
 import React from "react";
 import LogOut from "components/LogOut";
 import EditProfile from "components/EditProfile";
-
+import { useEffect } from "react";
+import { collection, onSnapshot, query } from "firebase/firestore";
+import { dbService } from "fBase";
+import { getAuth } from "firebase/auth";
+import { useState } from "react";
+import LongMovieCard from "components/LongMovieCard";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Profile = ({ userObj, refreshUser }) => {
+  const auth = getAuth();
+  const [myLists, setMyLists] = useState([]);
+
+  useEffect(() => {
+    const q = query(collection(dbService, auth.currentUser.uid));
+    onSnapshot(q, (snapshot) => {
+      const myListArr = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setMyLists(myListArr);
+    });
+  }, []);
 
   return (
     <div className="profileArea">
@@ -17,7 +36,15 @@ const Profile = ({ userObj, refreshUser }) => {
       </div>
       <div className="profileList">
         <h2>My List</h2>
-        
+        <Container className="mycardList">
+          <Row>
+            {myLists.map((list) => (
+              <Col lg={3}>
+                <LongMovieCard key={list.id} item={list} />
+              </Col>
+            ))}
+          </Row>
+        </Container>
       </div>
     </div>
   );
